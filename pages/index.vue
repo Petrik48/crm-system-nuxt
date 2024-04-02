42facef8d7d4d172a8b26ce289d8ba90954c6a2a
Petrik48 <alex.petrov.15@mail.ru>
2024-04-02 22:12:14 +0300

    2:52:27

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <script lang="ts" setup>
+  import type {ICard, IColumn} from "~/components/kanban/kanban.types";
+
+  useSeoMeta({
+    title: 'Home | CRM System',
+  })
+
+  const dragCardRef = ref<ICard | null>(null)
+  const sourceColumnRef = ref<IColumn | null>(null)
 
 </script>
 
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System by Petrik48</h1>
+    <div>
+      <UiCard class="mb-3" draggable="true">
+        <UiCardHeader role="button">name card</UiCardHeader>
+        <UiCardContent>Компания</UiCardContent>
+        <UiCardFooter>Date</UiCardFooter>
+      </UiCard>
+    </div>
   </div>
 </template>
