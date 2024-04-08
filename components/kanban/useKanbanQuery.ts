d537483d811332e3f98b3e6880d0c22d824453c4
Petrik48 <alex.petrov.15@mail.ru>
2024-04-08 11:48:59 +0300

    4:01:50

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -2,6 +2,7 @@ import {useQuery} from "@tanstack/vue-query";
 import {COLLECTION_DEALS, DB_ID} from "~/app.constants";
 import {KANBAN_DATA} from "~/components/kanban/kanban.data";
 import type {IDeal} from "~/types/deals.types";
+import type {IColumn} from "~/components/kanban/kanban.types";
 
 export function useKanbanQuery() {
 
@@ -10,10 +11,28 @@ export function useKanbanQuery() {
     queryKey: ['deals'],
     queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
     select(data) {
-      const newBoard = [...KANBAN_DATA]
+      const newBoard: IColumn[] = KANBAN_DATA.map(column => ({
+        ...column,
+        items: [],
+      }))
       const deals = data.documents as unknown as IDeal[]
 
+      for(const deal of deals) {
+        const column = newBoard.find(col => col.id === deal.status)
 
+        if (column) {
+          column.items.push({
+            $createdAt: deal.$createdAt,
+            id: deal.$id,
+            name: deal.name,
+            price: deal.price,
+            companyName: deal.customer.name,
+            status: column.name,
+          })
+        }
+      }
+
+      return newBoard
     }
   })
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <script lang="ts" setup>
   import type {ICard, IColumn} from "~/components/kanban/kanban.types";
   import {useKanbanQuery} from "~/components/kanban/useKanbanQuery";
+  import dayjs from "dayjs";
+  import {useMutation} from "@tanstack/vue-query";
+  import type {EnumStatus} from "~/types/deals.types";
+  import {COLLECTION_DEALS, DB_ID} from "~/app.constants";
+  import {generateColumnStyle} from "~/components/kanban/generate-gradient";
 
   useSeoMeta({
     title: 'Home | CRM System',
@@ -9,18 +14,81 @@
   const dragCardRef = ref<ICard | null>(null)
   const sourceColumnRef = ref<IColumn | null>(null)
 
-  useKanbanQuery()
+  const { data, isLoading, refetch } = useKanbanQuery()
+
+  type TypeMutationVariables = {
+    docId: string,
+    status?: EnumStatus,
+  }
+
+  const { mutate } = useMutation(({
+    mutationKey: ['move card'],
+    mutationFn: ({docId, status}:TypeMutationVariables) =>
+      DB.updateDocument(DB_ID, COLLECTION_DEALS, docId, {
+        status,
+      }),
+    onSuccess: () => {
+      refetch()
+    }
+  }))
+
+  function handleDragStart(card: ICard, column: IColumn) {
+    dragCardRef.value = card
+    sourceColumnRef.value = column
+  }
+
+  function handleDragOver(event: DragEvent) {
+    event.preventDefault()
+  }
+
+  function handleDrop(targetColumn: IColumn) {
+    if (dragCardRef.value && sourceColumnRef.value) {
+      mutate({docId: dragCardRef.value.id, status: targetColumn.id})
+    }
+  }
+
 </script>
 
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System by Petrik48</h1>
-    <div>
-      <UiCard class="mb-3" draggable="true">
-        <UiCardHeader role="button">name card</UiCardHeader>
-        <UiCardContent>Компания</UiCardContent>
-        <UiCardFooter>Date</UiCardFooter>
-      </UiCard>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+      <div class="grid grid-cols-5 gap-16">
+        <div
+            v-for="(column, index) in data"
+            :key="index"
+            class="min-h-screen"
+            @dragover="handleDragOver"
+            @drop="() => handleDrop(column)"
+        >
+          <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center" :style="generateColumnStyle(index, data?.length)">
+            {{ column.name }}
+          </div>
+          <div>
+            <KanbanCreateDeal :refetch="refetch" :status="column.id"/>
+            <UiCard
+                v-for="card in column.items"
+                :key="card.id"
+                class="mb-5"
+                draggable="true"
+                @dragstart="() => handleDragStart(card, column)"
+            >
+              <UiCardHeader role="button">
+                <UiCardTitle>{{ card.name }}</UiCardTitle>
+                <UiCardDescription class="mt-2 block">
+                  {{ convertCurrency(card.price) }}
+                </UiCardDescription>
+              </UiCardHeader>
+              <UiCardContent class="text-xs">
+                <div>Компания</div>
+                {{ card.companyName }}
+              </UiCardContent>
+              <UiCardFooter>{{ dayjs(card.$createdAt).format('DD MMMM YYYY') }}</UiCardFooter>
+            </UiCard>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
